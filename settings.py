1be4fe3466e7024a443dfa55611bf226a4e45707
saharovartur <saharovartur87@gmail.com>
2024-03-30 12:30:47 +0400

    /

---
+++ settings.py
@@ -0,0 +1,9 @@
+#Добавить установленную библиотеку в INSTALLED_APPS в файл settings проекта.
+#Лучше вынести в отдельную колонку.
+
+# 3rd Party
+'crispy_forms',
+
+#Добавить константу в конец файла settings
+#В моем проекте установлен bootstrap4.
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
